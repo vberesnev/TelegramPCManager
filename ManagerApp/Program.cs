6f2efe656fef73fc87927be52c2e116853edf3dc
vberesnev <vberesnev@spotware.com>
2020-06-28 14:56:52 +0300

    Initial commit

---
+++ ManagerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManagerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ManagerBL/TBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+
+namespace ManagerBL
+{
+    class TBot
+    {
+        TelegramBotClient _telegramBotClient;
+
+        public TBot(string token)
+        {
+            _telegramBotClient = new TelegramBotClient(token);
+
+        }
+        public void Start() { }
+
+        public void Stop() { }
+    }
+}
